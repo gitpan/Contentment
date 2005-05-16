5d20452f32880034edef791f171eed291da79140
Andrew Sterling Hanenkamp <hanenkamp@cpan.org>
2005-05-16 13:26:16 +0000

    Import of HANENKAMP/Contentment-0.009_004 from CPAN.
    gitpan-cpan-distribution: Contentment
    gitpan-cpan-version:      0.009_004
    gitpan-cpan-path:         HANENKAMP/Contentment/Contentment-0.009_004.tar.gz
    gitpan-cpan-author:       HANENKAMP
    gitpan-cpan-maturity:     developer

--- lib/Contentment.pm
+++ lib/Contentment.pm
@@ -4,24 +4,34 @@ use strict;
 use warnings;
 
 use Carp;
+use Contentment::Config;
+use Contentment::VFS;
 use Log::Log4perl ':easy';
 use YAML 'LoadFile';
 
-our $VERSION = '0.009_002';
-
-use constant ETC_DIR => '@@ETC_DIR@@';
+our $VERSION = '0.009_004';
 
 BEGIN {
 	Log::Log4perl::easy_init($DEBUG);
 }
 
-sub dc { "(".shift(@_).") (".shift(@_).":".shift(@_).") " }
-$SIG{__WARN__} = sub { Log::Log4perl::get_logger->warn(dc(caller),@_)  };
-$SIG{__DIE__}  = sub { eval { Log::Log4perl::get_logger->fatal(dc(caller),@_) }; confess @_; };
+sub dc { sprintf "(package %s) (file %s:line %d) (called %s)", @_ }
+sub scream {
+	my $i = 2;
+	my $str;
+	while (my @c = caller($i++)) {
+		$str .= sprintf "\tFrom package %s (%s:%d) called %s\n", @c;
+	}
+	return $str;
+}
+$SIG{__WARN__} = sub { Log::Log4perl::get_logger->warn(dc(caller(1)),"@_")  };
+$SIG{__DIE__}  = sub { eval { Log::Log4perl::get_logger->error("An error prevented Contentment from serving this request: @_\n ",scream) }; confess @_; };
+
+my $log = Log::Log4perl->get_logger(__PACKAGE__);
 
 =head1 NAME
 
-Contentment - This is the main module of Contentment
+Contentment - Contentment is a Mason/Perl-bsed CMS
 
 =head1 DESCRIPTION
 
@@ -31,15 +41,15 @@ General configuration information and some general-purpose methods can be found 
 
 =item $conf = Contentment->configuration
 
-Reads the configuration files F<ETC_DIR/Contentment.defaults.conf> and F<ETC_DIR/Contentment.conf>.
+Reads the configuration files F<Contentment::Config::ETC_DIR/Contentment.defaults.conf> and F<Contentment::Config::ETC_DIR/Contentment.conf>.
 
 =cut
 
 my %configuration;
 sub configuration {
 	unless (%configuration) {
-		my $defaults_file = ETC_DIR.'/Contentment.defaults.conf';
-		my $locals_file   = ETC_DIR.'/Contentment.conf';
+		my $defaults_file = Contentment::Config::ETC_DIR.'/Contentment.defaults.conf';
+		my $locals_file   = Contentment::Config::ETC_DIR.'/Contentment.conf';
 
 		my ($defaults, $locals);
 		eval {
@@ -81,6 +91,26 @@ sub run_plugin {
 
 	no strict 'refs';
 	return $plugin->(@_);
+}
+
+=item Contentment->call_hooks($dir, @args)
+
+Run the appropriate generator on all files in F</content/hooks/$dir> and all subdirectories. The given C<@args> are passed each time.
+
+Logs, but otherwise ignores, any errors that occur.
+
+=cut
+
+sub call_hooks {
+	my $class = shift;
+	my $dir   = shift;
+	my $vfs   = Contentment::VFS->new;
+
+	my $hook_dir = $vfs->lookup("/content/hooks/$dir");
+
+	for my $file ($vfs->find(sub { shift->has_content }, $hook_dir)) {
+		$file->generate(@_);
+	}
 }
 
 =back
